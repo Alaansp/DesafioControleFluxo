58832f60daca48e928aa9802d615fc26bc528cd7
Alan Mendes <110340422+Alaansp@users.noreply.github.com>
2024-05-18 14:18:11 -0300

    projeto pronto!

--- ControleFluxo/src/Contador.java
+++ ControleFluxo/src/Contador.java
@@ -1,11 +1,8 @@
 import java.util.Scanner;
 
 public class Contador {
-	public static void main(String[] args) {
-
-		//criando o objeto terminal
+	public static void main(String[] args) {	
 		Scanner terminal = new Scanner(System.in);
-
 		System.out.println("Digite o primeiro parâmetro");
 		int parametroUm = terminal.nextInt();
 		System.out.println("Digite o segundo parâmetro");
@@ -21,17 +18,17 @@ public class Contador {
 		}
 		
 	}
-	
+
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-		if (parametroUm >= parametroDois);
-		System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+		if (parametroUm >= parametroDois) {
+			System.out.println("O segundo parâmetro deve ser maior que o primeiro");
 		}
 		
 		int contagem = parametroDois - parametroUm;
 		//realizar o for para imprimir os números com base na variável contagem
-		for(int numeroContagem=1; numeroContagem<contagem; numeroContagem++) {
-			System.out.println("Imprimindo o número " + numeroContagem);
+		for (int i=1; i<contagem; i++) {
+			System.out.println("Imprimindo o número " + i);
 		}
 	}
 }
